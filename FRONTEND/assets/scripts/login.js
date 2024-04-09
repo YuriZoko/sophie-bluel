cc21add66181e1f48822dcbe611bdab708e5ea7d
YuriZoko <florian.girard07@orange.fr>
2024-04-09 10:54:30 +0200

    Update login.js

--- FRONTEND/assets/scripts/login.js
+++ FRONTEND/assets/scripts/login.js
@@ -1,58 +1,44 @@
-const errorBox = document.querySelector(".error-box"); 
+const errorBox = document.querySelector(".error-box");
 const emailField = document.getElementById("email-field");
 const passwordField = document.getElementById("password-field");
 const loginButton = document.getElementById("login-button");
-
-loginButton.addEventListener("click", () => {
-    let userCredentials = {
-        email: emailField.value,
-        password: passwordField.value
-    };
-    authenticateUser(userCredentials);
-})
-
-// Fonction d'authentification
-async function authenticateUser(userCredentials) {
+loginButton.addEventListener("click", authenticateUser);
+async function authenticateUser() {
+    const { value: email } = emailField;
+    const { value: password } = passwordField;
     errorBox.innerHTML = "";
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-z]{2,4}$/;
     const passwordPattern = /^[a-zA-Z0-9]+$/;
-
-    if (!emailPattern.test(userCredentials.email)) {
+    const createErrorParagraph = message => {
         const errorParagraph = document.createElement("p");
-        errorParagraph.innerHTML = "Veuillez entrer une adresse e-mail valide.";
+        errorParagraph.innerHTML = message;
         errorBox.appendChild(errorParagraph);
+    };
+    if (!emailPattern.test(email)) {
+        createErrorParagraph(`Veuillez entrer une adresse e-mail valide.`);
         return;
     }
-
-    if (!passwordPattern.test(userCredentials.password)) {
-        const errorParagraph = document.createElement("p");
-        errorParagraph.innerHTML = "Veuillez entrer un mot de passe valide.";
-        errorBox.appendChild(errorParagraph);
+    if (!passwordPattern.test(password)) {
+        createErrorParagraph(`Veuillez entrer un mot de passe valide.`);
         return;
     }
-
     try {
         const response = await fetch('http://localhost:5678/api/users/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
-            body: JSON.stringify(userCredentials)
+            body: JSON.stringify({ email, password })
         });
-        const result = await response.json();
-
-        if (result.error || result.message) {
-            const errorParagraph = document.createElement("p");
-            errorParagraph.innerHTML = "Combinaison e-mail/mot de passe incorrecte.";
-            errorBox.appendChild(errorParagraph);
-        } else if (result.token) {
-            localStorage.setItem("accessToken", result.token);
+        const { error, message, token } = await response.json();
+        if (error || message) {
+            createErrorParagraph(`Combinaison e-mail/mot de passe incorrecte.`);
+        } else if (token) {
+            localStorage.setItem("accessToken", token);
             window.location.href = "index.html";
         }
     } catch (error) {
         console.error("Une erreur est survenue lors de la tentative de connexion:", error);
-        const errorParagraph = document.createElement("p");
-        errorParagraph.innerHTML = "Une erreur s'est produite. Veuillez réessayer plus tard.";
-        errorBox.appendChild(errorParagraph);
+        createErrorParagraph(`Une erreur s'est produite. Veuillez réessayer plus tard.`);
     }
 }
